f120fa052c81d3441956ee62777a94c44e42f300
Snoop67 <arthurlebas@hotmail.fr>
2025-08-04 20:58:14 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// React component with corrected fraction calculation, reset, PDF and history features
+import React from 'react';
+
+function App() {
+  return <div style={{ padding: 20 }}>BED Simulator - Version Complète</div>;
+}
+
+export default App;
