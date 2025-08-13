38ed8e9d22f521950209458a7333b5d838f5911e
Snoop67 <arthurlebas@hotmail.fr>
2025-08-13 10:24:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useMemo, useState } from "react";
-import "./App.css";
+import "./index.css";
 
 /** ---------- Utilitaires ---------- */
 const parseNum = (v) => {
